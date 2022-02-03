5f22daf7d4ef9d8126a25ec09776b64a24a60510
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2022-02-03 11:20:15 -0600

    correccion de url back

--- src/api/backendApi.ts
+++ src/api/backendApi.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const FAKE_TOKEN = localStorage.getItem("fake_access_token");
 
-const baseURL = "http://tide.ecjasso.com/api/v1";
+const baseURL = "http://ecjasso.com/api/v1";
 
-export const imagesURL = "http://tide.ecjasso.com";
+export const imagesURL = "http://ecjasso.com";
 
 export const backendApi = axios.create({
   baseURL,
